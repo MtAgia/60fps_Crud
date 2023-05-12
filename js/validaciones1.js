f0f530781802a924fb19ab509409658bfd61ae9d
facundo cajal <facundocajal199@gmail.com>
2023-05-12 20:32:31 -0300

    validaciones y detalles casi terminados

--- js/validaciones1.js
+++ js/validaciones1.js
@@ -1,31 +1,40 @@
-const password = document.querySelector("#")
-const mail =document.querySelector("#")
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.getElementById("formulario");
+const warning = document.getElementById("warning")
 
-
-const admin = {
-  email:"facundocajal199@gmail.com",
-  password:"Mortadela05@",
-};
+form.addEventListener("submit", e=>{
+  e.preventDefault();
+  validarContrasenia();
+  validarEmail();
+})
 
 
 function validarContrasenia() {
-    const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\:;"'<>,.?/~`])(?=.{8,16})/;
-if (regEx.test(password.value)) {
-  password.className ="form-control is-valid"
-  return true ;
-} else {
-  password.className="form-control is-invalid"
-  return false;
-} 
+  const regExMail = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\:;"'<>,.?/~`])(?=.{8,16})/;
+  if (regExMail.test(password.value)) {
+    password.className = "form-control is-valid";
+    return true;
+  } else {
+    password.className = "form-control is-invalid";
+    
+    return false;
+  }
 }
 
 function validarEmail() {
-  const regEx =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  if (regEx.test(email.value)) {
+  const regExPass = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (regExPass.test(email.value)) {
     email.className = "form-control is-valid";
     return true;
   } else {
     email.className = "form-control is-invalid";
     return false;
   }
-} 
+}
+
+
+
+
+
+
