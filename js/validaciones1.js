b98b0838356f561486cc2b519cbaf501f8236493
facundo cajal <facundocajal199@gmail.com>
2023-05-13 15:20:42 -0300

    hot fix de la rama

--- js/validaciones1.js
+++ js/validaciones1.js
@@ -2,22 +2,34 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const form = document.getElementById("formulario");
 
+form.addEventListener("submit", (event) => {
+  let valid = true;
 
-form.addEventListener("submit", e=>{
-  e.preventDefault();
-  validarContrasenia();
-  validarEmail();
-})
+  if (!validarEmail()) {
+    valid = false;
+  }
 
+  if (!validarContrasenia()) {
+    valid = false;
+  }
+
+  if (!valid) {
+    event.preventDefault();
+  }
+});
 
 function validarContrasenia() {
   const regExMail = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\:;"'<>,.?/~`])(?=.{8,16})/;
   if (regExMail.test(password.value)) {
-    password.className = "form-control is-valid";
+    password.classList.remove("invalid");
+    password.classList.add("valid");
+    password.nextElementSibling.textContent = "";
     return true;
   } else {
-    password.className = "form-control is-invalid";
-    
+    password.classList.remove("valid");
+    password.classList.add("invalid");
+    password.nextElementSibling.textContent =
+      "La contraseña debe tener al menos 8 caracteres, incluyendo mayúsculas, minúsculas, números y símbolos especiales.";
     return false;
   }
 }
@@ -25,16 +37,15 @@ function validarContrasenia() {
 function validarEmail() {
   const regExPass = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (regExPass.test(email.value)) {
-    email.className = "form-control is-valid";
+    email.classList.remove("invalid");
+    email.classList.add("valid");
+    email.nextElementSibling.textContent = "";
     return true;
   } else {
-    email.className = "form-control is-invalid";
+    email.classList.remove("valid");
+    email.classList.add("invalid");
+    email.nextElementSibling.textContent =
+      "Por favor, ingrese una dirección de correo electrónico válida.";
     return false;
   }
 }
-
-
-
-
-
-
