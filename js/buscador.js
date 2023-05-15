d6ef662e154b3c78774f80d1bc3b2f2a686ecb68
Matias G <dikimatig@gmail.com>
2023-05-15 20:44:16 -0300

    acomodando buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,10 +1,10 @@
 //filtrado
 document.addEventListener(`keyup`, (e) => {
   if (e.target.matches(`.inputBuscar`)) {
-    document.querySelectorAll(`.juegos`).forEach((pelis) => {
-      pelis.textContent.toLowerCase().includes(e.target.value)
-        ? pelis.classList.remove(`filtro`)
-        : pelis.classList.add(`filtro`);
+    document.querySelectorAll(`.juegos`).forEach((juegos) => {
+      juegos.textContent.toLowerCase().includes(e.target.value)
+        ? juegos.classList.remove(`filtro`)
+        : juegos.classList.add(`filtro`);
     });
   }
 });
