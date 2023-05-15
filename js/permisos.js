41e87ae838cef20a60e364504c24cf92d28ed185
Silvio Ruiz Cormack <silviojrck@gmail.com>
2023-05-15 10:02:10 -0300

    agrega redireccion a pagina sin permisos

---
+++ js/permisos.js
@@ -0,0 +1,23 @@
+const contadorRedireccion = document.querySelector("#contador-redireccion");
+
+function redirigirAIndex() {
+    window.location.href = '../index.html';
+}
+  
+function actualizarContador(segundos) {
+    const contador = document.getElementById('contador-redireccion');
+    contador.textContent = segundos;
+}
+  
+function esperarRedireccion(segundos) {
+    if (segundos > 0) {
+        actualizarContador(segundos);
+        setTimeout(() => {
+        esperarRedireccion(segundos - 1);
+        }, 1000);
+    } else {
+        redirigirAIndex();
+    }
+}
+
+esperarRedireccion(5);
